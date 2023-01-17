8702ac2865abe285f21e055ddedfa584807708a6
Ronaldo Alencar <alencarprincipal@gmail.com>
2023-01-16 23:41:36 -0300

    atualização 1

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,44 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  const directors = moviesArray.map((currentMovie) => currentMovie.director);
+  return directors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  const dramaSpielberg = moviesArray.filter(
+    (currentMovie) =>
+      currentMovie.director === "Steven Spielberg" &&
+      currentMovie.genre.includes("Drama")
+  );
+  return dramaSpielberg.length;
+}
+console.log(howManyMovies(movies));
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  const average = moviesArray.reduce(
+    (acumulador, currentScore) => acumulador + currentScore.score,
+    0
+  );
+  return Number((average / moviesArray.length).toFixed(2));
+}
+console.log(scoresAverage(movies));
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  const dramatic = moviesArray.filter((currentMovie) =>
+    currentMovie.genre.includes("Drama")
+  );
+  const dramaticAverage = dramatic.reduce(
+    (acumulador, currentMovie) => acumulador + currentMovie.score,
+    0
+  );
+  return Number((dramaticAverage / dramatic.length).toFixed(2));
+}
+console.log(dramaMoviesScore(movies));
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
